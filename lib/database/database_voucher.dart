f22c8872fa9506343801252d7b380e7b94680ee9
raihantaufiq <raihantaufiqurrahman@upi.edu>
2023-01-05 02:32:03 +0700

    Update database_voucher.dart

--- lib/database/database_voucher.dart
+++ lib/database/database_voucher.dart
@@ -15,6 +15,40 @@ class MongoDatabase {
     return vouchers;
   }
 
+  static insertVoucherTetap(String name, String desc, int discount_price, int min_trans, String payment, String guide, DateTime expire_date){
+    voucherCollection.insertOne({
+      'name': name,
+      'terms&cond': [
+        {
+          'desc': desc,
+          'discount_price': discount_price,
+          'min_trans': min_trans,
+          'payment': payment
+        }
+      ],
+      'guide': guide,
+      'expire_date': expire_date
+    });
+  }
+
+  static insertVoucherPersen(String name, String desc, int discount_percent, int max_disc, int min_trans, String payment, String guide, DateTime expire_date){
+    voucherCollection.insertOne({
+      'name': name,
+      'terms&cond': [
+        {
+          'desc': desc,
+          'discount_percent': discount_percent,
+          'max_disc': max_disc,
+          'min_trans': min_trans,
+          'payment': payment
+        }
+      ],
+      'guide': guide,
+      'expire_date': expire_date
+    });
+  }
+
+
   // static insert(Voucher voucher) async {
   //   await voucherCollection.insertAll([voucher.toMap()]);
   // }
